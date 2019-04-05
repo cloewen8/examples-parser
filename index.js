cb43d662ebb2e24f694dfbd0d4fc90599419729b
Colton Loewen <loewencjay@outlook.com>
2019-04-05 02:04:28 -0600

    Created module entry point

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+const Glob = require('glob').Glob;
+const { EventEmitter } = require('events');
 
+/**
+ * @typedef {object} Example
+ */
+
+/**
+ * @typedef {object} ParserOptions
+ * May optionally include glob options.
+ */
+
+class Parser extends EventEmitter {
+	constructor(patterns, options) {
+		options = options || {};
+		super();
+		// Parse each pattern.
+		for (let pattern of patterns) {
+			let matcher = new Glob(pattern, options);
+			// let examples = [];
+			matcher.on('match', (file) => {
+				// todo: Parse file for examples.
+				// Save example to array.
+				// Emit example.
+				// Parse file examples.
+				console.log(file);
+			});
+			matcher.on('error', (err) => {
+				// todo: Emit error.
+				console.log(err);
+			});
+			matcher.on('end', () => {
+				matcher.removeAllListeners();
+				console.log('done');
+				// todo: Emit done.
+			});
+		}
+	}
+}
+
+module.exports = Parser;
